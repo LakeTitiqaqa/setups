97c80dd211056880e29d462461b33e61f5f35f3a
Gloria <122538489+LakeTitiqaqa@users.noreply.github.com>
2023-01-12 22:25:34 +0900

    Create add to path.sh

---
+++ installations/add to path.sh
@@ -0,0 +1,6 @@
+which python 
+# which python3 if you use python 3
+# /PATH/TO/YOUR/PYTHON/Versions/3.xx/bin/python3
+
+echo "alias python=//PATH/TO/YOUR/PYTHON/Versions/3.xx/bin/python3" >> ~/.bashrc
+# it may be >> ~/.zshrc depending on your shell type. 
